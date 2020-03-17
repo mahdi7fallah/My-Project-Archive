37ea7056c3c14ce1637bd914db91ee69fc872825
Mahdi Fallah <60651462+mahdi7fallah@users.noreply.github.com>
2020-03-17 20:22:08 +0330

    Create arduino_python_01.py

---
+++ arduino_python_01.py
@@ -0,0 +1,22 @@
+import time
+try:
+    from pyfirmata import Arduino as arduino, util
+except:
+    import pip
+    pip.main(['install', 'pyfirmata'])
+
+
+port = '/dev/ttyACM0'
+board = arduino(port)
+                                                    
+iterator = util.Iterator(board)
+iterator.start()
+
+motor = board.get_pin('d:6:p')
+poten = board.get_pin('a:0:i')
+
+for i in range(500):
+    motor.write(poten.read())
+    time.sleep(0.01)
+    print(i + 1, poten.read())
+a
